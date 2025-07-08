2c57c9981b34714800a69aafd5790624f96b5945
Jade Thomson <pacethomson@gmail.com>
2025-07-08 14:45:50 -0600

    EOD Tues 7/8

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,16 @@ import { OpenAiHandler } from './openai.mjs';
 
 
 async function main() {
-    // const movies = new MovieNight(RAPID_API_KEY);
-    // const res = await movies.tester();
-    // console.log("res", res);
-
+    
     const aiTHing = new OpenAiHandler(process.env.OPEN_AI_API_KEY);
-
+    
     const testTranscript = "I'm in the mood for something scary from the 80's";
-
-    aiTHing.getPreferencesFromTranscript(testTranscript);
-
+    
+    let aiResponse = aiTHing.getPreferencesFromTranscript(testTranscript);
+    
+    const movies = new MovieNight(process.env.RAPID_API_KEY);
+    const res = await movies.tester(aiResponse);
+    console.log("res", res);
 }
 
 
